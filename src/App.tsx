d4d84aab43c49673a9e5b1c241a0a044ea99fd43
robwatsongtr <rwatso@gmail.com>
2023-05-05 13:03:48 -0700

    added bootstrap, added react router, set up basic routeds

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Routes, Route, Navigate } from "react-router-dom"
+
 function App() {
-  return <h1>Hi</h1>
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Hi</h1>} />
+      <Route path="/new" element={<h1>New</h1>} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  )
 }
 
 export default App
